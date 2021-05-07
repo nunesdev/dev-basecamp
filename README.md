ea1cc3c45a3c44a4d15b9acba7ed05336d85ab12
bruno.reis <bruno.reis@empiricus.com.br>
2021-05-07 20:46:42 -0300

    run build

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# dev-basecamp
-Landing Page para o Basecamp "_init(dev) 2021"
+# README #
+
+# _init(DEV) Basecamp
+	Version: 0.0.1
+
+### How do I get set up? ###
+```
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:3001
+npm run dev
+```
+
+### How do I genarate build? ###
+```
+# build for homologation
+npm run build
+
+Obs: (this feature generate a builded directory called dist)
+```
+
+### OK! But... How i use this? ###
+```
+1 - Copy the root directory to your new project
+2 - npm install
+3 - npm run dev
+4 - Have fun.
+```
+
+#### How do I create new feature?
+	Clone the project, create a branch (feature/FEATURE_NAME) and next, create a pull request.
+
+
+### Who do I talk to? ###
+@brx.tech
---
+++ landingpage/dist/precache-manifest.a651522923b6c03bc0921c1caa072a4d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cd1ef1df8c24fb42d2",
+    "url": "/css/app.26854bad.css"
+  },
+  {
+    "revision": "6209dfa1e817412a51f37c5d4528d274",
+    "url": "/img/823jfd6vmwv21.jpg"
+  },
+  {
+    "revision": "10b80caf7ec120642fa6be931ce2014d",
+    "url": "/img/Arrows_blue.png"
+  },
+  {
+    "revision": "2865124b09878d0ecc868f254182d8be",
+    "url": "/img/IMG_2688.jpg"
+  },
+  {
+    "revision": "61f3a957d5c8ca2e9a883999db3e07d0",
+    "url": "/img/_MG_0070.JPG"
+  },
+  {
+    "revision": "e787e871730025d3440603e70b70a5e2",
+    "url": "/img/bg.jpg"
+  },
+  {
+    "revision": "cda210e4bf6c1fa84874ed86a73ef34a",
+    "url": "/img/blogger.png"
+  },
+  {
+    "revision": "00c85e1ff974dfc2ca62dcbc402a5956",
+    "url": "/img/certificado-digital.png"
+  },
+  {
+    "revision": "9e77f9d489fc3ad74f32090ae6922ff1",
+    "url": "/img/chat.png"
+  },
+  {
+    "revision": "0c89c179c5d15275a96650be2586c404",
+    "url": "/img/css.png"
+  },
+  {
+    "revision": "0d6ad22f860853a78b9004c05b3c8aec",
+    "url": "/img/discord.png"
+  },
+  {
+    "revision": "44563ff958e50632e1d8df74761abdee",
+    "url": "/img/dots-boot.png"
+  },
+  {
+    "revision": "af706637b2e15d170c6fa769090a8a1d",
+    "url": "/img/dots.png"
+  },
+  {
+    "revision": "01e500fca29c045d432b64f285f9c229",
+    "url": "/img/git.png"
+  },
+  {
+    "revision": "228cfd479343f07ea1a6260f3564ad65",
+    "url": "/img/html.png"
+  },
+  {
+    "revision": "9eec77ec6df95cba4f26c6f54622cd08",
+    "url": "/img/js.png"
+  },
+  {
+    "revision": "4542ad3342c6d6f03b68fc5d23c3830c",
+    "url": "/img/live.png"
+  },
+  {
+    "revision": "8b292ba7a5db1508a6c57303227c3ebf",
+    "url": "/img/mentor.png"
+  },
+  {
+    "revision": "a3d1ba4e1aa06e2051662ef6239b1f26",
+    "url": "/img/next.png"
+  },
+  {
+    "revision": "71cd3ec2e2b9aaeb131d94256bb3d2bb",
+    "url": "/img/rocket.png"
+  },
+  {
+    "revision": "ce83fa97ef1baa8ea5546b91f1bc6e77",
+    "url": "/img/telegram.png"
+  },
+  {
+    "revision": "6e0ae45ef04516b2e67116966f1ceda0",
+    "url": "/img/video-files.png"
+  },
+  {
+    "revision": "7e792481cb123d4385b0d6ba6bfb83eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc0a760ed4b676d8fe43",
+    "url": "/js/about.677a3670.js"
+  },
+  {
+    "revision": "78cd1ef1df8c24fb42d2",
+    "url": "/js/app.276bdd7a.js"
+  },
+  {
+    "revision": "04afe7f096856d6d1a6e",
+    "url": "/js/chunk-vendors.6b4af817.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "083e188c7dd308762ddd4540d4714f9c",
+    "url": "/svg/hexagon-dark.svg"
+  },
+  {
+    "revision": "4c1ecda2a1827e932856c32298e50e3c",
+    "url": "/svg/right-arrow.svg"
+  }
+]);
