3c095eb62364275baf66c0523ebedec04862921e
bruno.reis <bruno.reis@empiricus.com.br>
2021-05-25 17:58:10 -0300

    todolist project

---
+++ todolist/script.js
@@ -0,0 +1,57 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+const close = document.querySelectorAll(".close");
+
+close.forEach(function(item){
+  item.addEventListener('click', function(){
+    var div = this.parentElement;
+    div.style.display = "none";
+  })
+})
+
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function addTarefa() {
+  
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Você precisa inserir um título");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  close.forEach(function (item) {
+    item.addEventListener('click', function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    })
+  })
+}
